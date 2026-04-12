7a26eb28dea56af5e607b3bd7c014ae18121e104
simrangohel70-lab <simrangohel70@gmail.com>
2026-04-12 22:32:59 +0530

    build temprature converter

---
+++ program-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int celsius = 38;
+    int fahrenheit;
+
+    // Integer calculation
+    fahrenheit = (9 * celsius) / 5 + 32;
+
+    printf("Temperature in Fahrenheit: %d\n", fahrenheit);
+
+    return 0;
+}
